5eab70552c6adfce0819e9baec6affb27e8a5df9
Defarhan Nugraha <defarhannugraha1@gmail.com>
2023-03-23 19:32:06 +0700

    add response status

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
     
     const { method, url } = request;
 
@@ -26,13 +25,18 @@ const requestListener = (request, response) => {
 
     if (url === '/') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end(`Ini adalah homepage`);
+        } else {
+            response.statusCode = 400;
+            response.end(`Halaman tidak dapat diakses dengan ${method} request.`);
         }
-        response.end(`Halaman tidak dapat diakses dengan ${method} request.`);
+        
     } 
     
     if (url === '/about') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end(`Halo! ini adalah halaman about.`);
         }
 
@@ -46,13 +50,16 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const {name} = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`Halo, ${name}! ini adalah halaman about.`);
             });
             
          } else {
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${method} request.`);
          }
     } else {
+        response.statusCode = 404;
         response.end(`Halaman tidak ditemukan!`);
     }
 
